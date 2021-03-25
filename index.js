6de4c205c275faeb46d950dd2c9e333e182a583c
GlaucoSouza <gods.14@hotmail.com>
2021-03-25 12:57:18 +0000

    Second Commit, creation of js files

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http'),
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
+
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json());
+app.use(logger('tiny'));
+app.use(require('./routes'));
+
+let users = []; // names of users will be stored here
+(async function getNames(){
+  try{
+    const {data} = await axios.get("https://swapi.dev/api/people/");
+    console.log(data)
+    users = data.results.map(user=>user.name);
+    console.log(users);
+    } catch(error){
+    console.log(error)
+  }
+})()
