7f5dc6c9846251f445999cfecd54fb25a6a583d5
GlaucoSouza <gods.14@hotmail.com>
2021-04-13 15:16:45 +0000

    fixing some issues with db connection

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # WebAppCA2
 
+# Pet Shop Dublin
+
+Pet Shop Dublin website, where you can select one of our services from the service list and also register your pet's details like:
+name, if they are either a cat or a dog, the owner's name and the owner's phone.
+
 Create an API service (you must use Node.js and MongoDB) that supports fullCRUD for your existing CA1 app
 
 Update your existing CA1 app to allow populating the HTML table with JSON data provided by the API, implement the ability to Add and Delete items from that table using your new API only from your front-end. You are free to use any available frontend
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const http = require('http'),
-axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
 bodyParser = require('body-parser'),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+dotenv = require("dotenv");
 
 var app = express();
 var port = 8000;
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(logger('tiny'));
 app.use(require('./routes'));
 
-let users = []; // names of users will be stored here
-(async function getNames(){
-  try{
-    const {data} = await axios.get("https://swapi.dev/api/people/");
-    console.log(data)
-    users = data.results.map(user=>user.name);
-    console.log(users);
-    } catch(error){
-    console.log(error)
-  }
-})()
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
+
+const dbURI = process.env.DB_URL;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
