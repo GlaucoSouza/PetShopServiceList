b9b58c5683d6d82e649cb5d90e15864578bfd3a1
GlaucoSouza <gods.14@hotmail.com>
2021-04-20 08:11:01 +0000

    work inside index.js at the moment

--- index.js
+++ index.js
@@ -9,40 +9,43 @@ dotenv = require("dotenv");
 dotenv.config();
 var app = express();
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
+// const mainRoutes = require('./routes.js')
+// app.use(mainRoutes)
 
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "/")));
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/index.html")
 })
 
-app.use(bodyParser.urlencoded({extended: true}))
-// app.use(require('./routes'));
+// app.use(logger('tiny')); 
 
-//     //creation of the registration schema
-// const registrationSchema = {
-//     service: String,
-//     petname: String,
-//     animaltype: String,
-//     gender: String,
-//     ownername: String,
-//     email: { type: String, unique: true, lowercase: true},
-// }
 
-// const Register = mongoose.model("Register", registrationSchema)
+    //creation of the registration schema
+const registrationSchema = {
+    service: String,
+    petname: String,
+    animaltype: String,
+    ownername: String,
+    email: { type: String, unique: true, lowercase: true},
+}
 
-// app.post("/", (req, res)=>{
-//     let newRegister = new Register({
-//         service: req.body.service,
-//         petname: req.body.petname,
-//         animaltype: req.body.animaltype,
-//         gender: req.body.gender,
-//         ownername: req.body.ownername,
-//         email: req.body.email
-//     })
-//     newRegister.save()
-//     res.redirect('/')
-// })
+const Register = mongoose.model("Register", registrationSchema)
+
+app.post("/", (req, res)=>{
+    let newRegister = new Register({
+        service: req.body.service,
+        petname: req.body.petname,
+        animaltype: req.body.animaltype,
+        gender: req.body.gender,
+        ownername: req.body.ownername,
+        email: req.body.email
+    })
+    newRegister.save()
+    res.redirect('/')
+})
 
 app.listen(8000, ()=>{
     console.log("server is running on port 8000");
@@ -51,7 +54,7 @@ app.listen(8000, ()=>{
 //my db credentials are in a .env file stored with the name of dbURI
 const dbURI = process.env.DB_URL;
 
-//db connection
+//db connection using the dbURI credentials
 mongoose.connect(dbURI, {useNewUrlParser: true}, {useUnifiedTopology: true})
     .then((result) => console.log('connected to db'))
     .catch((err) => console.log(err));
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 router = express.Router();
 
-var registerCtrl = require('./register-controller');
+var registerCtrl = require('./register-controller').createRegister;
 
 //create a new registration to the service list
 router.post('/', registerCtrl.createRegister);
